3d01ec150594bbb3d5dd953849a3cf59b21a9f64
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-09-02 06:58:06 +0300

    added deleting skills and some styles

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -12,6 +12,12 @@
       <option value="designer">Web Designer</option>
     </select>
 
+    <label>Skills</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
+
     <div class="terms">
       <input type="checkbox" required v-model="terms" />
       <label>Accept terms and conditions</label>
@@ -47,7 +53,22 @@ export default {
       role: '',
       terms: false,
       names: [],
+      tempSkill: '',
+      skills: [],
     };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === ',' && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill.slice(0, -1));
+        }
+        this.tempSkill = '';
+      }
+    },
+    deleteSkill(skill) {
+      this.skills = this.skills.filter((item) => item !== skill);
+    },
   },
 };
 </script>
@@ -86,5 +107,17 @@ input[type='checkbox'] {
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
 }
 </style>
